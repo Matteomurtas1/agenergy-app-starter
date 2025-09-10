382788952710de5f60e8237d0f951df07dc1f41c
Matteomurtas1 <m.murtas@ag-energy.it>
2025-09-10 17:44:38 +0200

    Create supabaseBrowser.ts

---
+++ agenergy-app-starter/lib/lib/supabaseBrowser.ts
@@ -0,0 +1,8 @@
+// lib/supabaseBrowser.ts
+import { createClient } from '@supabase/supabase-js';
+
+export function supabaseBrowser() {
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+  const anonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+  return createClient(url, anonKey);
+}
